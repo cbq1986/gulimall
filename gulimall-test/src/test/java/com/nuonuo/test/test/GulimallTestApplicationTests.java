0438eeef4ded586c06455a04ab95aa2e8df81095
chibenqiang <chibenqiang@gmail.com>
2021-05-18 21:48:24 +0800

    项目创建

---
+++ gulimall-test/src/test/java/com/nuonuo/test/test/GulimallTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuonuo.test.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
